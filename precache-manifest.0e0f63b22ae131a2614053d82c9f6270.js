d926cd8669a424b548d20189c1027f5deb76002f
Daniil Davydov <dan0102dan@MacBook-Pro.local>
2020-11-18 14:48:40 +0300

    Updates

---
+++ precache-manifest.0e0f63b22ae131a2614053d82c9f6270.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341cf4c9335bbe73455abcca14f52b6c",
+    "url": "/newRelease/index.html"
+  },
+  {
+    "revision": "561ebd7a1b98e4702453",
+    "url": "/newRelease/static/css/2.62385eee.chunk.css"
+  },
+  {
+    "revision": "fa51c6dc1aa33a926a38",
+    "url": "/newRelease/static/css/main.3c30cfb9.chunk.css"
+  },
+  {
+    "revision": "561ebd7a1b98e4702453",
+    "url": "/newRelease/static/js/2.c09e8495.chunk.js"
+  },
+  {
+    "revision": "308550227c98d9d10aa54bd6a60fea4c",
+    "url": "/newRelease/static/js/2.c09e8495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa51c6dc1aa33a926a38",
+    "url": "/newRelease/static/js/main.14c7025b.chunk.js"
+  },
+  {
+    "revision": "324a9cc8252cce29e166",
+    "url": "/newRelease/static/js/runtime-main.762a49f9.js"
+  },
+  {
+    "revision": "cd5eac4280ad9ef77bb0a0fafd68f3fd",
+    "url": "/newRelease/static/media/AppleMusic.cd5eac42.svg"
+  },
+  {
+    "revision": "e359ad39480a3ed8d413d0a663bce8a7",
+    "url": "/newRelease/static/media/Spotify.e359ad39.svg"
+  },
+  {
+    "revision": "deac2f88018a907c02c1cdd6a640542e",
+    "url": "/newRelease/static/media/Yandex.deac2f88.svg"
+  },
+  {
+    "revision": "d549df088e53fea4eb24a8f66dbdc30b",
+    "url": "/newRelease/static/media/ava.d549df08.png"
+  },
+  {
+    "revision": "0b93970850799fc8c3d29c5a73b18d7e",
+    "url": "/newRelease/static/media/empty.0b939708.svg"
+  },
+  {
+    "revision": "6dd30fb5ff10b0c2164e59bc687ea4e8",
+    "url": "/newRelease/static/media/error.6dd30fb5.png"
+  }
+]);
